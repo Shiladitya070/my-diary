fa020df7f2d53e2faeaceb440f36d33cf2a4fb20
Antar Mukhopadhyaya <arunmukhopadhyaya96@gmail.com>
2021-05-03 11:00:35 +0530

    Major Changes

--- MyDiary/views.py
+++ MyDiary/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import redirect,render
-
+from note.models import Note
 
 
 def home(request):
-    return render(request,'home.html')
+    notes = Note.objects.order_by('-created_at')
+    context = {
+        'notes': notes
+    }
+    return render(request,'home.html',context=context)
+def search(request):
+    query = request.GET['query']
+    print(query)
+    return render(request,'search.html')
--- note/models.py
+++ note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from taggit.managers import TaggableManager
 # Create your models here.
+class Note (models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.TextField(max_length=500,default='')
+    content = models.TextField(max_length=10000,default='')
+    tags = TaggableManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+    
