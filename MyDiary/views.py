72f0a45ae50f8f16f77672e3039458f1a2e11d3c
shiladitya <shiladityad940@gmail.com>
2021-05-11 16:51:37 +0530

    tags improved

--- MyDiary/views.py
+++ MyDiary/views.py
@@ -43,11 +43,13 @@ def search(request):
     return render(request,'search.html',context)
 @login_required(login_url='/login/')
 def tagged(request, slug):
-    tag = get_object_or_404(Tag, slug=slug)
+    tag = get_object_or_404(Tag, slug=slug, )
+    print(tag)
     # Filter posts by tag name  
-    posts = Note.objects.filter(tags=tag)
+    posts = Note.objects.filter(tags=tag,author= request.user)
     context = {
         'tag':tag,
-        'posts':posts,
+        'notes':posts,
+        'color':['primary','secondary','success','danger', 'warning', 'info'],
     }
-    return render(request, 'home.html', context)
+    return render(request, 'note/tags.html', context)
