807829db101aa334b340c50bdd039dd2e909bb88
shiladitya <shiladityad940@gmail.com>
2021-06-22 18:06:58 +0530

    reinit

--- MyDiary/settings.py
+++ MyDiary/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +136,6 @@ SITE_ID = 1
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+django_heroku.settings(locals())
