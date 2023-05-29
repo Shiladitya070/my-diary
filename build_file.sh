e85ecb53c6d3509b51c3b09810368ec98b77dbaf
Shiladitya Das <shiladityad940@gmail.com>
2023-05-29 21:53:43 +0530

    deployment for verce test 3

--- build_file.sh
+++ build_file.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 echo "Installing dependencies..."
-python3.9 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 echo "done Installing :)"
 echo "Collecting static"
-python3.9 manage.py collectstatic  --noinput --clear    
-echo "Collecting static"
+python3 manage.py collectstatic  --noinput --clear    
+echo "Collecting static done :)"
+
+echo "making migrations"
+python3 manage.py makemigrations
+echo "migrate..."
+python3 manage.py migrate
+echo "done migartion :)"
+
 
 
 
